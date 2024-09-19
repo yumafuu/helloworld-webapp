126ba6c481120ef8dfaee848e7b85a3c79d38f6d
Yuma Ishikawa <yuma.fuu05@gmail.com>
2024-09-19 17:11:23 +0900

    [add] tcp connection check

--- main.go
+++ main.go
@@ -3,9 +3,53 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
+
+var (
+	// This is list of TCP connection that checked at the start of the application
+	// Env: TCP_CONNECTIONS
+	// Required: false
+	// Default: []string{}
+	tcpAddrs = []string{}
+
+	// This is the timeout for TCP connection check
+	// format: int
+	// Env: TCP_CONNECTION_TIMEOUT
+	// Default: 5
+	tcpTimeout = 5
+
+	// This is the port that the application will listen to
+	// Env: PORT
+	// Default: 8080
+	port = ""
+)
+
+func init() {
+	tcpAddrs = strings.Split(os.Getenv("TCP_CONNECTIONS"), ",")
+	timeoutStr := os.Getenv("TCP_CONNECTION_TIMEOUT")
+	if timeoutStr != "" {
+		var err error
+		tcpTimeout, err = strconv.Atoi(timeoutStr)
+		if err != nil {
+			log.Fatalf("Failed to convert timeout to int: %v", err)
+		}
+	}
+
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("TCP Connections: ", tcpAddrs)
+	fmt.Println("TCP Timeout: ", tcpTimeout)
+	fmt.Println("Port: ", port)
+}
 
 func printRequestDetail(r *http.Request) {
 	log.Printf("Request from %s: %s %s", r.RemoteAddr, r.Method, r.URL)
@@ -16,14 +60,15 @@ func main() {
 		printRequestDetail(r)
 		fmt.Fprintf(w, "Hello, World!")
 	})
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		printRequestDetail(r)
-		fmt.Fprintf(w, "I'm healthy!")
-	})
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	for _, addr := range tcpAddrs {
+		conn, err := net.DialTimeout("tcp", addr, time.Duration(tcpTimeout)*time.Second)
+
+		if err != nil {
+			log.Fatalf("Failed to connect to %s: %v", addr, err)
+		}
+		log.Printf("Connected to %s\n", addr)
+		defer conn.Close()
 	}
 
 	log.Printf("Listening on port %s", port)
